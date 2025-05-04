1286ac53b43bd9dde2169ddee53d934525636a13
user1 <vukelan@gmail.com>
2025-05-04 16:23:58 +0300

    get logs from path

--- src/GetLogs.cs
+++ src/GetLogs.cs
@@ -1,9 +1,41 @@
+using System;
 using System.Text.Json;
+using System.IO;
 
 namespace GetLogsFile;
 
-public class GetLogs {
-  public string GetLogs(string urlParam) {
-    
-  }
+public class GetLogs
+{
+    private string filePath;
+    private bool isRead;
+
+    public GetLogs()
+    {
+        filePath = Environment.GetEnvironmentVariable("DOT_NET_DIR");
+        if (!String.IsNullOrEmpty(filePath))
+        {
+            filePath = "README.md";
+            isRead = false;
+        }
+        else
+        {
+            isRead = true;
+        }
+    }
+
+    public string OnGetLogs(string urlParam)
+    {
+        try
+        {
+            using StreamReader reader = new(filePath);
+            string content = reader.ReadToEnd();
+
+            if (isRead) { }
+            return content;
+        }
+        catch (IOException e)
+        {
+            return e.GetType().Name;
+        }
+    }
 }
