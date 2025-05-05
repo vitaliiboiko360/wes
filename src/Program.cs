990e2bdfd46821ced46e95ac5187897073e137cb
user1 <vukelan@gmail.com>
2025-05-06 00:57:01 +0300

    add logs route

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using GetParamsFile;
+using GetLogsFile;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -6,8 +7,14 @@ var app = builder.Build();
 
 var getParams = new GetParams();
 
+var getLogs = new GetLogs();
+
 app.MapGet("/{*urlParam}", (string urlParam = "") => {
   return getParams.Get(urlParam);
+});
+
+app.MapGet("/logs", () => {
+  return getLogs.OnGetLogs();
 });
 
 app.Run();
